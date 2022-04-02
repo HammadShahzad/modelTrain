6dfe5b65f2b2b38ee259ccf177c75144e3ebfc6b
Hammad Shahzad <hammadshahzad3420@gmail.com>
2022-04-02 11:25:55 +0500

    Update model.py

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ def Encoder(df):
 df = Encoder(df).dropna()
 
 #find x='All Data' and y='Previous GPA'
-y=df.iloc[:,50]
+y=df.iloc[:,49]
 x=df.drop(columns=['Please mention your Previous Semester GPA?'],axis=1)
 
 #Train Data Split
@@ -28,7 +28,10 @@ X_train, X_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,random_s
 #Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
 
-"""# Decision Tree Regressor"""
+
+""" **Complete Data Set Regression** """
+
+""" **Decision Tree Regressor - Complete Dataset**"""
 
 from sklearn import tree
 clf = tree.DecisionTreeRegressor()
@@ -37,7 +40,25 @@ y_pred = clf.predict(X_test)
 
 from sklearn import metrics
 r_sqaure=metrics.r2_score(y_test,y_pred)
-print(r_sqaure)
+# print(r_sqaure)
+
+"""## **MultiVariable Linear Regression - Complete Dataset**"""
+
+
+for column in df:
+  from sklearn.metrics import mean_squared_error
+  from sklearn.metrics import r2_score
+  from sklearn.linear_model import LinearRegression
+  import numpy as np
+
+  regression = LinearRegression()
+  regression.fit(x,y)
+  predicted = regression.predict(x)
+  mse = mean_squared_error(y, predicted)
+  r2 = r2_score(y, predicted)
+  #print(column, "MSE:", mse, "RMSE:", np.sqrt(mse),"R-Squared:", r2)
+  print(column, "RMSE:", round(np.sqrt(mse), 4),'\t', "R-Squared:", round(r2,4) , '\n')
+  
 
 
 
